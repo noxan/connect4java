e3c0e39394ba25d39584a0a1f795662094dfd509
andre.kupka@augustakom.net <andre.kupka@augustakom.net@011b0be2-c168-11de-bb5c-85734917f5ce>
2010-01-09 13:42:13 +0000

    v0.8.18 AbstractCard replaced by generic type in AbstractListener
    git-svn-id: http://connect4java.googlecode.com/svn/trunk@18 011b0be2-c168-11de-bb5c-85734917f5ce

--- src/com/googlecode/connect4java/gui/listener/AbstractListener.java
+++ src/com/googlecode/connect4java/gui/listener/AbstractListener.java
@@ -1,24 +1,26 @@
 package com.googlecode.connect4java.gui.listener;
 
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import com.googlecode.connect4java.gui.card.AbstractCard;
 
+
 /**
  * 
  * @author richard.stromer
- * @version 0.8.17
+ * @version 0.8.18
  * @since 0.6.12
- *
+ * 
  * @param <E>
  */
 public abstract class AbstractListener<E extends AbstractCard> implements ActionListener {
-	protected AbstractCard card;
-	
-	public AbstractListener(E e) {
-		card = e;
-	}
-	
-	public abstract void actionPerformed(ActionEvent e);
+    protected E card;
+
+    public AbstractListener(E e) {
+	card = e;
+    }
+
+    public abstract void actionPerformed(ActionEvent e);
 }
--- src/com/googlecode/connect4java/pref/Version.java
+++ src/com/googlecode/connect4java/pref/Version.java
@@ -1,21 +1,23 @@
 package com.googlecode.connect4java.pref;
 
+
 /**
  * 
  * @author richard.stromer
- * @version 0.8.17
+ * @version 0.8.18
  * @since 0.2
  */
 public class Version {
-	public static final int VERSION_MAIN = 0;
-	public static final int VERSION_SUB = 8;
-	public static final int VERSION_REVSION = 17;
-	
-	public static String string() {
-		return VERSION_MAIN+"."+VERSION_SUB+"."+VERSION_REVSION;
-	}
-	@Override
-	public String toString() {
-		return "Version["+VERSION_MAIN+"."+VERSION_SUB+"."+VERSION_REVSION+"]";
-	}
+    public static final int VERSION_MAIN = 0;
+    public static final int VERSION_SUB = 8;
+    public static final int VERSION_REVSION = 18;
+
+    public static String string() {
+	return VERSION_MAIN+"."+VERSION_SUB+"."+VERSION_REVSION;
+    }
+
+    @Override
+    public String toString() {
+	return "Version["+VERSION_MAIN+"."+VERSION_SUB+"."+VERSION_REVSION+"]";
+    }
 }
